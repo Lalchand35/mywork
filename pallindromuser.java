8fb1bec7780d824c535557fc4ac03fbcabfd5053
Lalchand35 <114717687+Lalchand35@users.noreply.github.com>
2023-04-26 13:16:40 +0530

    Add files via upload

---
+++ pallindromuser.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class pallindromuser 
+{
+public static void main(String[]args)
+{
+ Scanner sc=new Scanner(System.in);
+ System.out.println("enter the number");
+ int num=sc.nextInt();
+ int rem,rev=0;
+ int temp=num;
+ while(temp!=0)
+ {
+     rem=temp%10;
+     rev=rev*10+rem;
+    temp=temp/10;
+ }
+ if(num==rev)
+ {
+     System.out.println("is pallindrone");
+ }
+ else{
+     System.out.println("is not pallindrone");
+ }
+ sc.close();   
+}
+}
