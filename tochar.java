708f0ebb0fa26df93809c425e34a2d6bb15d7a09
Lalchand35 <114717687+Lalchand35@users.noreply.github.com>
2023-04-27 14:00:56 +0530

    Add files via upload

---
+++ tochar.java
@@ -0,0 +1,14 @@
+
+public class tochar 
+{
+public static void main(String[]args)
+{
+    String str = "Hello";
+    for(int i=0;i<str.length();i++)
+    {
+        char ch=str.charAt(i);
+        System.out.println("character at"+i+"position"+ch);
+    }
+}
+
+}
